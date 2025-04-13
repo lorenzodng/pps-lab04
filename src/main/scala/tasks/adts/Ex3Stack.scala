e53d405999331d3028ab5034f125b00b82a117fa
lorenzodng <104213722+lorenzodng@users.noreply.github.com>
2025-04-13 20:03:45 +0200

    Update Ex3Stack.scala

--- src/main/scala/tasks/adts/Ex3Stack.scala
+++ src/main/scala/tasks/adts/Ex3Stack.scala
@@ -16,19 +16,25 @@ object Ex3Stacks:
   trait StackADT:
     type Stack[A]
     def empty[A]: Stack[A] 
+
     extension [A](stack: Stack[A])
+
       def push(a: A): Stack[A]
       def pop(): Optional[(A, Stack[A])]
       def asSequence(): Sequence[A]
 
   object StackImpl extends StackADT:
+
     type Stack[A] = Sequence[A]
+
     def empty[A]: Stack[A] = Sequence.Nil()
     
     extension [A](stack: Stack[A])
       
       def push(a: A): Stack[A] = Sequence.Cons(a, stack)
+
       def pop(): Optional[(A, Stack[A])] = stack match
         case Sequence.Cons(head, tail) => Optional.Just(head, tail)
         case _ => Optional.Empty()
+
       def asSequence(): Sequence[A] = stack
