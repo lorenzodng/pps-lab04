668c76f1d8a74a6ad619f5b763efa9c38f824c60
lorenzodng <104213722+lorenzodng@users.noreply.github.com>
2025-04-13 20:43:02 +0200

    Update Ex4Summables.scala

--- src/main/scala/tasks/typeclasses/Ex4Summables.scala
+++ src/main/scala/tasks/typeclasses/Ex4Summables.scala
@@ -32,6 +32,7 @@ object Ex4Summables:
 
   def sumAll[A: Summable](seq: Sequence[A]) =
     val summable = summon[Summable[A]]
+    @tailrec
     def sumLoop(acc: A, seq: Sequence[A]): A = seq match
       case Nil() => acc
       case Cons(head, tail) => summable.sum(summable.sum(head, acc), sumLoop(acc, tail))
